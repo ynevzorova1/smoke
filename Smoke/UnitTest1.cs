12a7a68338c6e8551a76f5d30cdb813cffc4d140
Yulia Nevzorova <ynevzorova@gmail.com>
2019-03-20 10:48:41 +0200

    fix

--- Smoke/UnitTest1.cs
+++ Smoke/UnitTest1.cs
@@ -1,52 +1,76 @@
 ﻿using NUnit.Framework;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
-using System.Threading;
+using System;
 
-namespace Smoke
+namespace QaLight
 {
-    public class OldQaLight
-            {
+    public class UnitTest1
+    {
+        IWebDriver driver;
 
-                IWebDriver driver;
-        
-                    [SetUp]
-                public void SetUp()
-                {
-                    driver = new ChromeDriver();
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Navigate().GoToUrl("http://old.qalight.com.ua/zapisatsya-na-kursy.html");
-            }
+        }
 
-                    [Category("Smoke")]
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
 
-                    [Test]
+        [Category("Smoke")]
+        [Test]
+        public void SmokeTest()
+        {
+            string expectedElementLocator = ".alert.alert-error";
 
-                    public void SmokeTest()
-                {
-
-            IWebElement course = driver.FindElement(By.CssSelector("[name='_7c8289bf6b8e80c1749ef54ab01b92b8']"));
+            IWebElement course = driver.FindElement(By.CssSelector("[name = '_7c8289bf6b8e80c1749ef54ab01b92b8']"));
             SelectElement courseDropdown = new SelectElement(course);
             courseDropdown.SelectByIndex(3);
 
-                    IWebElement surnameField = driver.FindElement(By.Id("z_sender0"));
-                    surnameField.SendKeys("Surname");
+            IWebElement surnameField = driver.FindElement(By.Id("z_sender0"));
+            surnameField.SendKeys("Surname");
 
-                    IWebElement nameField = driver.FindElement(By.Id("z_text1"));
-                    nameField.SendKeys("Name");
+            IWebElement nameField = driver.FindElement(By.Id("z_text1"));
+            nameField.SendKeys("Name");
 
             IWebElement phoneNumber = driver.FindElement(By.Id("z_text0"));
-            phoneNumber.SendKeys("80503322587");
+            phoneNumber.SendKeys("067112233");
 
-            Thread.Sleep(2000);
-                }
+            IWebElement emailField = driver.FindElement(By.Id("z_sender1"));
+            emailField.SendKeys("afdsafsa@ukr.net");
 
-        public bool IsElementPresent(string cssSelector,IWebDriver driver)
+            IWebElement skypeField = driver.FindElement(By.Id("z_text2"));
+            skypeField.SendKeys("dsafasfassf");
+
+            IWebElement sourceInfo = driver.FindElement(By.CssSelector("[name = '_e926ba2b2813f56de8fc13877057e908']"));
+            SelectElement sourceInfoDropdown = new SelectElement(sourceInfo);
+            sourceInfoDropdown.SelectByIndex(4);
+
+            IWebElement submit = driver.FindElement(By.CssSelector("[type = submit]"));
+            submit.Click();
+
+            //IWebElement successElement = driver.FindElement(By.CssSelector(".alert.alert-success"));
+
+            //IWebElement errorElement = driver.FindElement(By.CssSelector(".alert.alert-error"));
+
+            Assert.True(IsElementPresent(driver, expectedElementLocator),
+                $"Element with locator {expectedElementLocator} is not present on the page");
+
+        }
+
+        public bool IsElementPresent(IWebDriver driver, string cssSelector)
         {
+            var elements = driver.FindElements(By.CssSelector(cssSelector));
 
-           var elements = driver.FindElement(By.CssSelector(cssSelector)); 
-            if (elements.Count ==1)
+            if (elements.Count == 1)
             {
                 return true;
             }
@@ -56,11 +80,8 @@ namespace Smoke
             }
             else
             {
-                throw new Exception ;
+                throw new Exception("Unexpected exception");
             }
-
-            Assert.True(IsElementPresent(driver, expectedElementLocator),
         }
-                }
-            }
-        
+    }
+}
